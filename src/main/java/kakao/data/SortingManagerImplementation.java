87ea1c375c00759b3d4c4cc959120ccc37af7bab
superchangho <superchangho@gmail.com>
2015-08-12 09:38:14 +0900

    i love u

--- src/main/java/kakao/data/SortingManagerImplementation.java
+++ src/main/java/kakao/data/SortingManagerImplementation.java
@@ -1,12 +1,23 @@
 package kakao.data;
 
 
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Vector;
+
+import com.hazelcast.nio.serialization.Data;
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
 import kakao.redis.RedisEvalHandler;
 import kakao.redis.RedisResultCode;
 import kakao.redis.listdatahandler;
@@ -16,6 +27,7 @@ public class SortingManagerImplementation implements SortingManager, listdatahan
 	private Vertx vertx;
 	private StorageManager storagemanager;
 	private HashMap<String, String> Result = new HashMap<String, String>();
+	private HashMap<String, sortingData> Data = new HashMap<String, sortingData>();
 	
 	
 	@Override
@@ -65,11 +77,45 @@ public class SortingManagerImplementation implements SortingManager, listdatahan
 		
 		vertx.executeBlocking(codeHandler, resultHander);
 	}
-
+	
+	class LikeComparator implements Comparator
+	{
+		@Override
+		public int compare(Object a, Object b) 
+		{
+			return ((sortingData)a).like > ((sortingData)b).like ? 1: 0;
+		}
+		
+	}
+	
+	public java.util.List<sortingData> changeToList(HashMap<String, sortingData> map)
+	{
+		java.util.List<sortingData> list = new ArrayList<sortingData>();
+		for(Iterator<Map.Entry<String, sortingData>> i = map.entrySet().iterator(); i.hasNext();)
+		{
+			Map.Entry<String, sortingData> entry = i.next();
+			list.add(entry.getValue());
+		}
+		
+		Collections.sort(list, new LikeComparator());
+		
+		return list;
+	}
+	
+	
 	@Override
 	public void OnListDataReadComplete(HashMap<String, String> result) 
 	{
-		this.Result = result;
+		Data.clear();
+		for(Iterator<Map.Entry<String, String>> i = result.entrySet().iterator(); i.hasNext();)
+		{
+			Map.Entry<String, String> entry = i.next();
+			JsonObject test = new JsonObject();
+			sortingData tData = Json.decodeValue(entry.getValue(), sortingData.class);
+			Data.put(tData.url, tData);
+		}
+		
+		changeToList(Data);
 	}
 
 }
---
+++ src/main/java/kakao/data/sortingData.java
@@ -0,0 +1,12 @@
+package kakao.data;
+
+public class sortingData 
+{
+	public sortingData() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String title;
+	public String url;
+	public long like;
+}
