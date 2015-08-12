c80b904a18e093a25f9cdc65c99585e3bbf4f8f9
kongji <ahdlrhtkk@naver.com>
2015-08-12 11:44:10 +0900

    add static main method to start application easily.

--- src/main/java/kakao/main/KakaoApp.java
+++ src/main/java/kakao/main/KakaoApp.java
@@ -10,7 +10,6 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -30,64 +29,51 @@ import org.jsoup.nodes.Document;
 
 import com.google.gson.Gson;
 
-
 /**
  * @author <a href="http://tfox.org">Tim Fox</a>
  */
-public class KakaoApp extends AbstractVerticle implements RedisReadHandler 
-{
+public class KakaoApp extends AbstractVerticle implements RedisReadHandler {
 	private HttpServer[] applicationServers;
 	private StorageManager storagemanager;
 	private SortingManager sortingmanager;
-	
-		
-	public void start()
-	{			
+
+	public void start() {
 		initializeLogger();
 		Log.Info("Server Initialize");
 		initializeStoragemanager();
 		initializeSortingmanager();
 	}
-	
-	void initializeLogger()
-	{
+
+	void initializeLogger() {
 		Logger logger = LoggerFactory.getLogger("kakao");
 		Log.Init(logger);
 	}
-	
-	private class timerHandler implements Handler<Long>
-	{
+
+	private class timerHandler implements Handler<Long> {
 		@Override
-		public void handle(Long timerID) 
-		{
+		public void handle(Long timerID) {
 			sortingmanager.Sorting();
 		}
 	}
-	
+
 	private timerHandler timer;
-	
-	void initializeSortingmanager()
-	{
+
+	void initializeSortingmanager() {
 		sortingmanager = new SortingManagerImplementation();
 		sortingmanager.Init(vertx, storagemanager);
-		
-		if(timer == null)
-		{
+
+		if (timer == null) {
 			timer = new timerHandler();
 			vertx.setPeriodic(commonData.Sorting_Period, timer);
 		}
 	}
-	
-	void initializeStoragemanager()
-	{
+
+	void initializeStoragemanager() {
 		Log.Info("Initialize Storagemanager");
 		storagemanager = new StorageManagerImplementation();
-		try
-		{
+		try {
 			storagemanager.Init(vertx);
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			Log.Exception(e);
 			Log.Fatal("Fail to initialize storagemanager");
 			return;
@@ -95,131 +81,106 @@ public class KakaoApp extends AbstractVerticle implements RedisReadHandler
 		createWasServer();
 		Log.Info("Storagemanager created");
 	}
-	
-	private void createWasServer()
-	{
+
+	private void createWasServer() {
 		applicationServers = new HttpServer[commonData.ServerCount];
-		
-		for(int i = 0 ; i < commonData.ServerCount ; ++i)
-		{
+
+		for (int i = 0; i < commonData.ServerCount; ++i) {
 			applicationServers[i] = vertx.createHttpServer();
 			AddHandler(applicationServers[i]);
 		}
 	}
-	
-	private void AddHandler(HttpServer server)
-	{
-		server.requestHandler(new Handler<HttpServerRequest>() 
-		{
+
+	private void AddHandler(HttpServer server) {
+		server.requestHandler(new Handler<HttpServerRequest>() {
 
 			@Override
-			public void handle(HttpServerRequest request) 
-			{
-				switch (request.path()) 
-				{
-				case "/":
-					indexpage(request);
-				break;
-				case "/input.html":
-					inputpage(request);
-				break;
-				case "/saveurl.json":
-					try 
-					{
-						savepage(request);
-					} 
-					catch (Exception e) 
-					{
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+			public void handle(HttpServerRequest request) {
+				try {
+					switch (request.path()) {
+						case "/":
+							indexpage(request);
+							break;
+						case "/input.html":
+							inputpage(request);
+							break;
+						case "/saveurl.json":
+							savepage(request);
+							break;
+						case "/getlist.json":
+							getList(request);
+							break;
+						default:
+							break;
 					}
-				break;
-				case "/getlist.json":
-					try 
-					{
-						getList(request);
-					} 
-					catch (Exception e) 
-					{
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				break;
-				default:
-					break;
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
 			}
-			
+
 		}).listen(8181);
 	}
-	
-	private void savepage(HttpServerRequest request) throws Exception
-	{
+
+	private void savepage(HttpServerRequest request) throws Exception {
 		MultiMap mMap = request.params();
 		Document doc = Jsoup.connect(mMap.get("url")).get();
-		
 
 		String title = doc.title();
-		
+
 		JsonObject data = new JsonObject();
 		data.put("title", title);
 		data.put("url", mMap.get("url"));
 		data.put("like", 0);
 
-		RedisWriteHandler writeHandler = new RedisWriteHandler() 
-		{
+		RedisWriteHandler writeHandler = new RedisWriteHandler() {
 			@Override
-			public void OnWriteSuccess(Object context) 
-			{
+			public void OnWriteSuccess(Object context) {
 				Log.Info("WriteSuccess");
 			}
-			
+
 			@Override
-			public void OnWriteSuccess(boolean newField, Object context) 
-			{
+			public void OnWriteSuccess(boolean newField, Object context) {
 				Log.Info("WriteSuccess");
 			}
-			
+
 			@Override
-			public void OnWriteError(RedisResultCode errorcode, String errorMessage, Object context) 
-			{
+			public void OnWriteError(RedisResultCode errorcode, String errorMessage, Object context) {
 				Log.Info(String.format("OnWriteError %s", errorMessage));
 			}
 		};
-		
+
 		storagemanager.GetMasterDB().WriteIfNotExistInHashMap("list", mMap.get("url"), data.toString(), writeHandler, null);
 	}
-	
-	private void getList(HttpServerRequest request) throws Exception
-	{
+
+	private void getList(HttpServerRequest request) throws Exception {
 		List<sortingData> sortedList = sortingmanager.getSortedList();
 		HttpServerResponse response = request.response();
 		String json = new Gson().toJson(sortedList);
 		response.end(json);
 	}
-	
-	private void inputpage(HttpServerRequest request)
-	{
+
+	private void inputpage(HttpServerRequest request) {
 		request.response().sendFile("webroot/input.html");
 	}
-	
-	private void indexpage(HttpServerRequest request)
-	{
+
+	private void indexpage(HttpServerRequest request) {
 		request.response().sendFile("webroot/index.html");
 	}
 
 	@Override
-	public void OnReadSuccess(HashMap<String, String> values, Object context) 
-	{
+	public void OnReadSuccess(HashMap<String, String> values, Object context) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void OnReadError(RedisResultCode errorCode, String errormessage, Object context) 
-	{
+	public void OnReadError(RedisResultCode errorCode, String errormessage, Object context) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
+	public static void main(String[] args) throws Exception {
+		io.vertx.core.Starter.main(new String[]{"run", KakaoApp.class.getName(), "vertx.options.blockedThreadCheckInterval", "2147483647"});
+	}
+
 }
